cced0ff30265643505f4010d925bfcefcd4c8fd1
Egor <e.arkhiptsev@gmail.com>
2023-06-06 00:27:59 +0300

    fix imports

--- api_yamdb/api/v1/serializers.py
+++ api_yamdb/api/v1/serializers.py
@@ -5,7 +5,6 @@ from django.core.exceptions import ValidationError
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
-
 from reviews.models import Category, Comment, Genre, Review, Title
 
 User = get_user_model()
