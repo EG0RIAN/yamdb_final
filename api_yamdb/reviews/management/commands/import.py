421725f1aaebdffb44e2b4ae5b8304f4e86abd15
Egor <e.arkhiptsev@gmail.com>
2023-06-06 00:26:12 +0300

    fix imports

--- api_yamdb/reviews/management/commands/import.py
+++ api_yamdb/reviews/management/commands/import.py
@@ -1,13 +1,12 @@
 import csv
-import os
 import logging
+import os
 
-from django.core.management.base import BaseCommand
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
 
-from reviews.models import (GenreTitle, Category, Genre, Title,
-                            Review, Comment)
+from reviews.models import Category, Comment, Genre, GenreTitle, Review, Title
 
 User = get_user_model()
 
